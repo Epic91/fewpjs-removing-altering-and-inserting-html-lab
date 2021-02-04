808bfd4fbf2d9f849f67ba1fbc4a3ed2485749dd
Epic91 <hernandeze791@gmail.com>
2021-02-03 21:51:05 -0600

    commit tree cb3a0bf44e4e4f9279ff1bba8d9ac50e96072a26

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
-const testVar = "hi"
-
 /* Enter the code to remove the node element here */
 document.querySelector("main#main").remove();
 
 /* Create your new element here and assign it to newHeader */
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory"
+// newHeader.innerHTML = "Flatiron School is the champion!";
+// newHeader.className = "victory";
+
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = "Flatiron School is the champion!";
-newHeader.className = "victory";
+newHeader.innerHTML = "Evelyn is the champion!"
